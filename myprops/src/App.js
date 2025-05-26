9bf892b0a988881da87364d181a85ee073d874b4
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-26 03:54:50 -0700

    rendering Lists

---
+++ myprops/src/App.js
@@ -0,0 +1,115 @@
+import extensions from './assets/extensions.avif'
+import css from './assets/css.png';
+import speed from './assets/speed.jpg';
+import json from './assets/json.png'
+import view from './assets/view.png';
+import mark from './assets/mark.jpg';
+import grid from './assets/grid.png';
+import dow from './assets/dow.png';
+import link from './assets/link.png';
+import dom from './assets/dom.jpg';
+import console from './assets/console.png'
+import devlens from './assets/devlens.png';
+
+const data =[
+    {
+        name:"DevLens",
+        use: "Quickly inspect page layouts and visualise elements boundaries",
+        photoName: devlens,
+        status:"active"
+    },
+    {
+        name: "StlyeSpy",
+        use:"Instantly analyse and copy Css from any webpage element",
+        photoName: css,
+        status:"active",
+    },
+    {
+      name:"SpeedBoost",
+      use:"Optimizes browser resource usage to accelerate page loading.",
+      photoName: speed,
+      status:"inactive"
+    },
+    {
+        name:" JSONWizard",
+        use:"Formats, validate and prettifies JSON responses in-browers",
+        photoName: json,
+        status:"active",
+    },
+    {
+        name:" TabMaster Pro",
+        use:"Organises browers tabs into groups and sessions",
+        photoName: dow,
+        status: "active",
+    },
+    {
+        name:"View PortBuddy",
+        use:"simulates various screen resolutions directly within the browser",
+        photoName: view,
+        status:"inactive"
+    },
+    {
+        name:"Markup Notes",
+        use:"Enables annotation and notes directly onto webpages for collaboratives debugging",
+        photoName: mark,
+        status:"active"
+    },
+    {
+        name:"GridGuides",
+        use:"Overlay customize grids and alignment guides to any webpage.",
+        photoName:grid,
+        status:"inactive"
+    },
+    {
+        name:"LinkerChecker",
+        use:"Scans and highlights broken on any page",
+        photoName:link,
+        status:"active"
+    },
+    {
+        name:"DOM Snapchat",
+        use:"Capture and export Dom structures quickly",
+        photoName: dom,
+        status:"inactive"
+    },
+    {
+        name:"Console Plus",
+        use:"Enhance developer console with advance filter and logging",
+        photoName: console,
+        status:"active"
+    }
+]
+
+function App() {
+  return (
+    <div>
+     < Lists />
+ </div>
+  );
+}
+
+export default App;
+
+
+function Lists(){
+    return(
+        <div>
+            {data.map((extension)=>(
+                <ExtensionLists name={extension.name}
+                   use={extension.use}
+                />
+            ))}
+        </div>
+    )
+}
+
+function ExtensionLists(props){
+  return(
+    <div>
+    <li>
+      <h1>{props.name}</h1>
+      <p>{props.use}</p>
+    </li>
+    </div>
+  )
+}
